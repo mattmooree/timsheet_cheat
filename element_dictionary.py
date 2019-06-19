01ae2c56d8d74c2e759a3df95f108dd8bca3e442
Matthew Moore <matthew.moore@inov8consulting.co.uk>
2019-06-19 10:46:32 +0100

    Adding the guts

---
+++ element_dictionary.py
@@ -0,0 +1,10 @@
+class ElementDictionary:
+    def __init__(self):
+        self.element_dict = {
+            "monday_drop": """//*[@id="ctlEnd_1_0_0"]""",
+            "tuesday_drop": """//*[@id="ctlEnd_1_1_0"]""",
+            "wednesday_drop": """//*[@id="ctlEnd_1_2_0"]""",
+            "thursday_drop": """//*[@id="ctlEnd_1_3_0"]""",
+            "friday_drop": """//*[@id="ctlEnd_1_4_0"]""",
+            "save_draft_btn": """"//*[@id="MainContent_uc_0_tblSubmit_1_pnlFooterButtons_btnSaveTaskCode_CC_-1"]"""
+        }
---
+++ inov8_login.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from element_dictionary import ElementDictionary
+import argparse
+
+
+class TimesheetFiller:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.url = "https://inov8consulting.timesheetportal.com/"
+        self.element_dict = ElementDictionary().element_dict
+        self.driver = webdriver.Chrome()
+
+    def fill_in_timesheet(self):
+        self.driver.get(self.url)
+        self.driver.find_element_by_name("txtUsername").send_keys(self.username)
+        self.driver.find_element_by_name("txtAuthToken").click()
+        self.driver.find_element_by_name("txtAuthToken").send_keys(self.password + Keys.ENTER)
+        days = [x for x in self.element_dict if x.__contains__("day")]
+        for day in days:
+            select = Select(self.driver.find_element_by_xpath(self.element_dict[day]))
+            select.select_by_visible_text('1')
+        self.driver.find_element_by_xpath(self.element_dict["save_draft_btn"]).click()
+        self.driver.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username', required=True, help="Username for timesheets login")
+    parser.add_argument('password', required=True, help="Password for timesheets login")
+    args = parser.parse_args()
+    timesheet_filler = TimesheetFiller(args.username, args.password)
+    timesheet_filler.fill_in_timesheet()
+
+
+if __name__ == "__main__":
+    main()
